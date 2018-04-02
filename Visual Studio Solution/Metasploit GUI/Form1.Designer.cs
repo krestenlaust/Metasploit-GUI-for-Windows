66e88f2d478235b4f5f43ce90dece9a1791f77dd
Nett18 <kressckerl@gmail.com>
2018-04-02 17:07:54 +0200

    Solution

---
+++ Visual Studio Solution/Metasploit GUI/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Metasploit_GUI
+{
+    partial class Installer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.metasploitstatus = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pythonwrapperstatus = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.metasploitstatus);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 148);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Metasploit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download and Install";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // metasploitstatus
+            // 
+            this.metasploitstatus.AutoSize = true;
+            this.metasploitstatus.Location = new System.Drawing.Point(6, 62);
+            this.metasploitstatus.Name = "metasploitstatus";
+            this.metasploitstatus.Size = new System.Drawing.Size(86, 17);
+            this.metasploitstatus.TabIndex = 2;
+            this.metasploitstatus.Text = "Not installed";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pythonwrapperstatus);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(216, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(198, 148);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Python-Wrapper";
+            // 
+            // pythonwrapperstatus
+            // 
+            this.pythonwrapperstatus.AutoSize = true;
+            this.pythonwrapperstatus.Location = new System.Drawing.Point(6, 62);
+            this.pythonwrapperstatus.Name = "pythonwrapperstatus";
+            this.pythonwrapperstatus.Size = new System.Drawing.Size(86, 17);
+            this.pythonwrapperstatus.TabIndex = 2;
+            this.pythonwrapperstatus.Text = "Not installed";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Download and Install";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Status:";
+            // 
+            // Installer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Installer";
+            this.Text = "Metasploit GUI - Installer";
+            this.Load += new System.EventHandler(this.Installer_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label metasploitstatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label pythonwrapperstatus;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Visual Studio Solution/Metasploit GUI/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Metasploit_GUI
+{
+    public partial class Installer : Form
+    {
+        public string systemroot = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System));
+        public bool metasploit;
+        public bool pythonwrapper;
+
+        public Installer()
+        {
+            InitializeComponent();
+        }
+
+        private void Installer_Load(object sender, EventArgs e)
+        {
+            UpdateDownloads();
+        }
+
+        public void UpdateDownloads()
+        {
+            //if (File.Exists(Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System) + "Metasploit-framework")))
+            if (File.Exists(systemroot + "Metasploit-framework"))
+            {
+                metasploit = true;
+                metasploitstatus.Text = "Installed";
+            }
+            else
+            {
+                metasploit = false;
+                metasploitstatus.Text = "Not installed";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://windows.metasploit.com/metasploitframework-latest.msi");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(systemroot + @"metagui\Extensions\Python-Enwrapper")){
+                //Isnt installed
+            }
+            else
+            {
+                //Is installed
+            }
+        }
+    }
+}
