13554d98422c4ced039d5c7887f0b6a0af6111b4
Nett18 <kressckerl@gmail.com>
2018-04-08 21:24:47 +0200

    Updated the Script Editor

--- Visual Studio Solution/Metasploit GUI/ScriptEditor.cs
+++ Visual Studio Solution/Metasploit GUI/ScriptEditor.cs
@@ -12,6 +12,10 @@ namespace Metasploit_GUI
 {
     public partial class ScriptEditor : Form
     {
+        public string[] GreenWords = new string[1];
+        public string[] PurpleWords = new string[2];
+        public string[] BlueWords = new string[1];
+        public string[] Words;
         public ScriptEditor()
         {
             InitializeComponent();
@@ -19,12 +23,47 @@ namespace Metasploit_GUI
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
+            for (int i = 0; i < richTextBox1.Lines.Length; i++)
+            {
+                string text = richTextBox1.Lines[i];
+                richTextBox1.Select(richTextBox1.GetFirstCharIndexFromLine(i), text.Length);
+                richTextBox1.SelectionColor = colorForLine(text);
+            }
+            /*
+            Words = richTextBox1.Text.Split(' ');
+            for (int i = 0; i < Words.Length; i++)
+            {
 
+            }*/
+
+            richTextBox1.DeselectAll();
+            richTextBox1.BackColor = Color.White;
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            
         }
 
         private void ScriptEditor_Load(object sender, EventArgs e)
         {
-
+            PurpleWords[0] = "exploit";
+            PurpleWords[1] = "run";
+            BlueWords[0] = "use";
         }
-    }
+
+        private Color colorForLine(string line)
+        {
+            //if (line.Contains("[INFO]")) return Color.Green; //if (line.Contains("[INFO]", StringComparison.InvariantCultureIgnoreCase)) return Color.Green;
+            //if (line.Contains("[ERROR]")) return Color.Red;
+            for(int i = 0; i < PurpleWords.Length; i++)
+            {
+                if (line.Contains(PurpleWords[i])) return Color.Magenta;
+            }
+            for(int i = 0; i < BlueWords.Length; i++)
+            {
+                if (line.Contains(BlueWords[i])) return Color.SkyBlue;
+            }
+            return Color.Black;
+        }
+
+
+}
 }
